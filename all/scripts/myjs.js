653bc62bf64824d3fe7039e93e2d6a8e2fcf1710
maliksuhail <maliksuhail.nestle@gmail.com>
2023-09-24 10:17:20 +0500

    ''

--- all/scripts/myjs.js
+++ all/scripts/myjs.js
@@ -1,5 +1,95 @@
 document.getElementById('mydiv').style.display='none';
 function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo8(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_8_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeoX(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_X_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Plus(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Plus_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Ultra(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Ultra_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo6(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_6_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS9(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S9_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo5(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_5_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS6(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S6_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Pro(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Pro_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS3(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S3_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixHot30(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Hot_30_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixHot30i(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Hot_30i_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixNote30Pro(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Note_30_Pro_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixNote30(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Note_30_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixHot30Play(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Hot_30_Play_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixSmart7HD(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Smart_7_HD_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixSmart7(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Smart_7_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixHot20i(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Hot_20i_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixZero20(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Zero_20_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixNote12VIP(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Note_12_VIP_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixNote12G96(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Note_12_G96_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixNote12(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Note_12_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixHot12(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Hot_12_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixHot12i(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Hot_12i_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixHot12Play(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Hot_12_Play_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixSmart6Plus(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Smart_6_Plus_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixZero5G(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Zero_5G_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixNote11Pro(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Note_11_Pro_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function InfinixHot11Play(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/infinix/Infinix_Hot_11_Play_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
 function SamsungZFlip5(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/samsung/Samsung_Galaxy_Z_Flip_5_specs.jpg';window.scrollTo(0,0);}
 document.getElementById('mydiv').style.display='none';
 function hidediv(){document.getElementById('mydiv').style.display='none';}
