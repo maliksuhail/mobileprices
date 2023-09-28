b6b48de871fb16e1800ed2a2dbd4e8cf173432fa
maliksuhail <maliksuhail.nestle@gmail.com>
2023-09-28 15:48:48 +0500

    ''

---
+++ all/scripts/myjs.js
@@ -0,0 +1,132 @@
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo8(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_8_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeoX(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_X_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Plus(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Plus_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Ultra(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Ultra_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo6(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_6_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS9(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S9_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo5(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_5_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS6(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S6_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Pro(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Pro_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS3(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S3_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo8(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_8_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeoX(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_X_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Plus(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Plus_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Ultra(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Ultra_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo6(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_6_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS9(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S9_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo5(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_5_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS6(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S6_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Pro(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Pro_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS3(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S3_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo8(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_8_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo8(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_8_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo8(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_8_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeoX(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_X_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Plus(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Plus_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Ultra(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Ultra_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo6(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_6_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS9(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S9_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo5(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_5_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS6(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S6_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Pro(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Pro_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo8(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_8_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeoX(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_X_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Plus(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Plus_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Ultra(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Ultra_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo6(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_6_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS9(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S9_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo5(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_5_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS6(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S6_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7Pro(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_Pro_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxNeo7(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_Neo_7_specs.jpg';window.scrollTo(0,0);}
+document.getElementById('mydiv').style.display='none';
+function hidediv(){document.getElementById('mydiv').style.display='none';}
+function SparxS3(){myImage=document.getElementById('my-image');document.getElementById('mydiv').style.display='block';myImage.style.width='100%';myImage.style.height='auto';myImage.src='https://maliksuhail.github.io/mobileprices/sparx/Sparx_S3_specs.jpg';window.scrollTo(0,0);}
